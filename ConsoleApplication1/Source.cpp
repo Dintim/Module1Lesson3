665418854f927e167b61f46de43fab7acdb14c73
ТилектесА <@238_4>
2018-05-22 10:00:44 +0600

    Add project files.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+		/*printf("5 10\t 100t\t x25\n");
+		printf("7cm\t 1949v\t xy\n");*/
+
+	/*int x = 0;
+	printf("введите x: ");
+	scanf("%d", &x);
+
+	int y = 0;
+	y = 7 * (x*x) - (3 * x) - 16;
+
+	printf("y = %d\n", y);*/
+
+
+	/*int a = 0;
+	printf("введите а: ");
+	scanf("%d", &a);
+
+	int x = 0;
+	x = 12 * (a*a) + (7 * a) - 16;
+
+	printf("x = %d\n", x);*/
+
+
+	/*int a = 0;
+	printf("введите а = ");
+	scanf("%d", &a);
+
+	int p = 0;
+	p = 4 * a;
+
+	printf("периметр равен %d\n", p);*/
+
+
+	/*int r = 0;
+	printf("введите r: ");
+	scanf("%d", &r);
+
+	int d = 0;
+	d = 2 * r;
+
+	printf("диаметр равен %d\n", d);*/
+
+}
